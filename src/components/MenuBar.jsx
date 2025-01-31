e7f5517faabe72e471aaeec3f9537ec5061585b0
sunkyoung <yskkjdh@gmail.com>
2025-01-31 15:39:44 +0900

    design : 메뉴바 tag -> icon으로 변경

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -10,85 +10,98 @@ import {
   FaUnderline,
   FaUndo,
 } from "react-icons/fa";
+import styles from "styles/menubar.module.scss";
 
 const MenuBar = ({ editor }) => {
-  if (!editor) {
-    return null;
-  }
-
   return (
-    <div className="menuBar">
-      <div>
+    <div className={styles.toolbar}>
+      {/*heading*/}
+      <div className={styles.itemBox}>
         <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive("bold") ? "is_active" : ""}
-        >
-          <FaBold />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive("italic") ? "is_active" : ""}
-        >
-          <FaItalic />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          className={editor.isActive("underline") ? "is_active" : ""}
-        >
-          <FaUnderline />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={editor.isActive("strike") ? "is_active" : ""}
-        >
-          <FaStrikethrough />
-        </button>
-        <button
+          type="button"
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
-          className={
-            editor.isActive("heading", { level: 2 }) ? "is_active" : ""
-          }
-        >
-          <FaHeading />
-        </button>
+          className={`${styles.toolbarBtn} ${styles.h1} ${
+            editor.isActive("heading", { level: 2 })
+              ? styles.active
+              : styles.none
+          }`}
+        ></button>
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
-          className={
-            editor.isActive("heading", { level: 3 }) ? "is_active" : ""
-          }
-        >
-          <FaHeading className="heading3" />
-        </button>
+          className={`${styles.toolbarBtn} ${styles.h2} ${
+            editor.isActive("heading", { level: 3 })
+              ? styles.active
+              : styles.none
+          }`}
+        ></button>
+      </div>
+      {/*text formatting*/}
+      <div className={styles.line} />
+      <div className={styles.itemBox}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={`${styles.toolbarBtn} ${styles.bold} ${
+            editor.isActive("bold") ? styles.active : styles.none
+          }`}
+        />
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={`${styles.toolbarBtn} ${styles.italic} ${
+            editor.isActive("italic") ? styles.active : styles.none
+          }`}
+        ></button>
+        <button
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          className={`${styles.toolbarBtn} ${styles.underline} ${
+            editor.isActive("underline") ? styles.active : styles.none
+          }`}
+        ></button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={`${styles.toolbarBtn} ${styles.strike} ${
+            editor.isActive("strike") ? styles.active : styles.none
+          }`}
+        ></button>
+      </div>
+      <div className={`${styles.line} `} />
+      <div className={styles.itemBox}>
         <button
           onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive("bulletList") ? "is_active" : ""}
-        >
-          <FaListUl />
-        </button>
+          className={`${styles.toolbarBtn} ${styles.bulleted} ${
+            editor.isActive("bulletList") ? styles.active : styles.none
+          }`}
+        ></button>
         <button
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editor.isActive("orderedList") ? "is_active" : ""}
-        >
-          <FaListOl />
-        </button>
+          className={`${styles.toolbarBtn} ${styles.numbered} ${
+            editor.isActive("orderedList") ? styles.active : styles.none
+          }`}
+        ></button>
         <button
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editor.isActive("blockquote") ? "is_active" : ""}
-        >
-          <FaQuoteLeft />
-        </button>
+          className={`${styles.toolbarBtn} ${styles.quote} ${
+            editor.isActive("blockquote") ? styles.active : styles.none
+          }`}
+        ></button>
       </div>
-      <div>
-        <button onClick={() => editor.chain().focus().undo().run()}>
-          <FaUndo />
-        </button>
-        <button onClick={() => editor.chain().focus().redo().run()}>
-          <FaRedo />
-        </button>
+      <div className={styles.line} />
+      <div className={styles.itemBox}>
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          className={`${styles.toolbarBtn} ${styles.undo} ${
+            editor.isActive("blockquote") ? styles.active : styles.none
+          }`}
+        ></button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          className={`${styles.toolbarBtn} ${styles.redo} ${
+            editor.isActive("blockquote") ? styles.active : styles.none
+          }`}
+        ></button>
       </div>
     </div>
   );
