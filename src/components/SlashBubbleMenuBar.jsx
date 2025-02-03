dde5915434104ac12f26eafa7f9416c199c5a5a8
SunKyoung Yoon <yskkjdh@gmail.com>
2025-02-04 00:32:00 +0900

    design : slashbubblemenubar 버튼 디자인  (이미지, 제목, 본문)형태로 변경

--- src/components/SlashBubbleMenuBar.jsx
+++ src/components/SlashBubbleMenuBar.jsx
@@ -1,14 +1,6 @@
+import styles from "styles/slashmenubar.module.scss";
+
 import { BubbleMenu } from "@tiptap/react";
-import {
-  FaBold,
-  FaHeading,
-  FaItalic,
-  FaListOl,
-  FaListUl,
-  FaQuoteLeft,
-  FaStrikethrough,
-  FaUnderline,
-} from "react-icons/fa";
 import ColorList from "components/ColorList";
 
 const SlashBubbleMenuBar = ({ editor }) => {
@@ -23,64 +15,92 @@ const SlashBubbleMenuBar = ({ editor }) => {
         }}
         shouldShow={() => true}
       >
-        <div className="bubble-menu">
+        <div className={styles.slashbar}>
           <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            className={editor.isActive("bold") ? "is-active" : ""}
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            className={`${styles.slashbarBtn} ${
+              editor.isActive("heading", { level: 1 })
+                ? styles.active
+                : styles.none
+            }`}
           >
-            bold
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            className={editor.isActive("italic") ? "is-active" : ""}
-          >
-            <FaItalic />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleStrike().run()}
-            className={editor.isActive("strike") ? "is-active" : ""}
-          >
-            <FaStrikethrough />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleUnderline().run()}
-            className={editor.isActive("underline") ? "is-active" : ""}
-          >
-            <FaUnderline />
+            <div className={`${styles.btnImage} ${styles.h1}`}></div>
+            <div className={styles.btnContent}>
+              <h3 className={styles.btnTitle}>제목1</h3>
+              <p className={styles.btnDesc}>섹션 제목(대)</p>
+            </div>
           </button>
           <button
             onClick={() =>
               editor.chain().focus().toggleHeading({ level: 2 }).run()
             }
-            className={editor.isActive("heading2") ? "is-active" : ""}
+            className={`${styles.slashbarBtn} ${
+              editor.isActive("heading", { level: 2 })
+                ? styles.active
+                : styles.none
+            }`}
           >
-            <FaHeading />
+            <div className={`${styles.btnImage} ${styles.h2}`}></div>
+            <div className={styles.btnContent}>
+              <h3 className={styles.btnTitle}>제목2</h3>
+              <p className={styles.btnDesc}>섹션 제목(중)</p>
+            </div>
           </button>
           <button
             onClick={() =>
               editor.chain().focus().toggleHeading({ level: 3 }).run()
             }
-            className={editor.isActive("heading3") ? "is-active" : ""}
+            className={`${styles.slashbarBtn} ${
+              editor.isActive("heading", { level: 3 })
+                ? styles.active
+                : styles.none
+            }`}
           >
-            <FaHeading className="heading3" />
+            <div className={`${styles.btnImage} ${styles.h3}`}></div>
+            <div className={styles.btnContent}>
+              <h3 className={styles.btnTitle}>제목3</h3>
+              <p className={styles.btnDesc}>섹션 제목(소)</p>
+            </div>
           </button>
           <button
             onClick={() => editor.chain().focus().toggleBulletList().run()}
-            className={editor.isActive("bulletList") ? "is-active" : ""}
+            className={`${styles.slashbarBtn} ${
+              editor.isActive("bulletList") ? styles.active : styles.none
+            }`}
           >
-            <FaListUl />
+            <div className={`${styles.btnImage} ${styles.bulleted}`}></div>
+            <div className={styles.btnContent}>
+              <h3 className={styles.btnTitle}>글머리 기호 목록</h3>
+              <p className={styles.btnDesc}>
+                간단한 글머리 기호 목록을 생성하세요.
+              </p>
+            </div>
           </button>
           <button
             onClick={() => editor.chain().focus().toggleOrderedList().run()}
-            className={editor.isActive("orderedList") ? "is-active" : ""}
+            className={`${styles.slashbarBtn} ${
+              editor.isActive("orderedList") ? styles.active : styles.none
+            }`}
           >
-            <FaListOl />
+            <div className={`${styles.btnImage} ${styles.numbered}`}></div>
+            <div className={styles.btnContent}>
+              <h3 className={styles.btnTitle}>번호 매기기 목록</h3>
+              <p className={styles.btnDesc}>번호가 매겨진 목록을 생성하세요.</p>
+            </div>
           </button>
           <button
             onClick={() => editor.chain().focus().toggleBlockquote().run()}
-            className={editor.isActive("blockquote") ? "is-active" : ""}
+            className={`${styles.slashbarBtn} ${
+              editor.isActive("blockquote") ? styles.active : styles.none
+            }`}
           >
-            <FaQuoteLeft />
+            <div className={`${styles.btnImage} ${styles.quote}`}></div>
+            <div className={styles.btnContent}>
+              <h3 className={styles.btnTitle}>인용</h3>
+              <p className={styles.btnDesc}>인용문을 작성하세요. </p>
+            </div>
           </button>
           <div>
             <ColorList editor={editor} />
